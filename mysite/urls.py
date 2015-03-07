9672ccc229cc94d4a064853d362e5cea48bc6ae5
Brandy Bannister <brandy@solidys.com>
2015-03-06 21:19:36 -0600

    landing page redirects to polls!

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 urlpatterns = patterns('',
-
-    url(r'^$', include('polls.urls', namespace = 'polls')),
+    url(r'^$', RedirectView.as_view(url="/polls/", permanent =False), name="index"),
+    url(r'^polls/', include('polls.urls', namespace = 'polls')),
     url(r'^admin/', include(admin.site.urls)),
 )
